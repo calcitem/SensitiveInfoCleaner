a2a195eafc40f6a0364d67f11febc58ef5041b5f
Calcitem <calcitem@outlook.com>
2023-03-26 23:02:23 +0800

    Format

--- gui/WinSensitiveInfiCleaner/main.cpp
+++ gui/WinSensitiveInfiCleaner/main.cpp
@@ -1,14 +1,14 @@
 // WinSensitiveInfiCleaner.cpp : Defines the entry point for the application.
 //
 
-#include "framework.h"
 #include "WinSensitiveInfiCleaner.h"
+#include "framework.h"
 
+#include <codecvt>
 #include <fstream>
+#include <locale>
 #include <string>
 #include <thread>
-#include <codecvt>
-#include <locale>
 #include <windows.h>
 
 #define ID_CLEANUP_BTN 1
@@ -20,9 +20,10 @@ void RunSensitiveInfoCleaner();
 
 HWND inputBox, outputBox;
 
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args,
+                   int ncmdshow)
 {
-    WNDCLASSW wc = { 0 };
+    WNDCLASSW wc = {0};
 
     wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
     wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
@@ -33,12 +34,11 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
     if (!RegisterClassW(&wc))
         return -1;
 
-    CreateWindowW(
-        L"myWindowClass", L"Sensitive Info Cleaner",
+    CreateWindowW(L"myWindowClass", L"Sensitive Info Cleaner",
                   WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 1800, 1000,
                   nullptr, nullptr, nullptr, nullptr);
 
-    MSG msg = { 0 };
+    MSG msg = {0};
     while (GetMessage(&msg, nullptr, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
@@ -50,24 +50,19 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
     switch (msg) {
     case WM_CREATE: {
-        CreateWindowW(
-            L"button", L"Cleanup",
-            WS_VISIBLE | WS_CHILD,
-            500, 500, 100, 25,
-            hWnd, (HMENU)ID_CLEANUP_BTN, nullptr, nullptr);
+        CreateWindowW(L"button", L"Cleanup", WS_VISIBLE | WS_CHILD, 500, 500,
+                      100, 25, hWnd, (HMENU)ID_CLEANUP_BTN, nullptr, nullptr);
 
-        inputBox = CreateWindowW(L"edit", L"",
-                                 WS_VISIBLE | WS_CHILD | WS_BORDER |
-                                     ES_MULTILINE | WS_VSCROLL,
-                                 50, 50, 800, 400,
-                                 hWnd, (HMENU)ID_INPUT_BOX, nullptr, nullptr);
+        inputBox = CreateWindowW(
+            L"edit", L"",
+            WS_VISIBLE | WS_CHILD | WS_BORDER | ES_MULTILINE | WS_VSCROLL, 50,
+            50, 800, 400, hWnd, (HMENU)ID_INPUT_BOX, nullptr, nullptr);
 
         outputBox = CreateWindowW(L"edit", L"",
                                   WS_VISIBLE | WS_CHILD | WS_BORDER |
                                       ES_MULTILINE | WS_VSCROLL | ES_READONLY,
-                                  900, 50, 800,
-                                  400,
-                                  hWnd, (HMENU)ID_OUTPUT_BOX, nullptr, nullptr);
+                                  900, 50, 800, 400, hWnd, (HMENU)ID_OUTPUT_BOX,
+                                  nullptr, nullptr);
 
         HFONT hFont = CreateFont(12, 0, 0, 0, FW_NORMAL, 0, 0, 0,
                                  DEFAULT_CHARSET, OUT_DEFAULT_PRECIS,
@@ -97,7 +92,7 @@ void RunSensitiveInfoCleaner()
 {
     // Save inputBox content to input.txt in UTF-8 format
     int inputLength = GetWindowTextLengthW(inputBox) + 1;
-    wchar_t* inputContentW = new wchar_t[inputLength];
+    wchar_t *inputContentW = new wchar_t[inputLength];
     GetWindowTextW(inputBox, inputContentW, inputLength);
 
     std::wstring_convert<std::codecvt_utf8<wchar_t>> utf8_conv;
@@ -115,10 +110,10 @@ void RunSensitiveInfoCleaner()
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
 
-    if (!CreateProcessA(
-            "SensitiveInfoCleaner.exe", nullptr, nullptr, nullptr, FALSE, 0,
-            nullptr, nullptr, &si, &pi)) {
-        MessageBoxA(nullptr, "Failed to run SensitiveInfoCleaner.exe", "Error", MB_OK | MB_ICONERROR);
+    if (!CreateProcessA("SensitiveInfoCleaner.exe", nullptr, nullptr, nullptr,
+                        FALSE, 0, nullptr, nullptr, &si, &pi)) {
+        MessageBoxA(nullptr, "Failed to run SensitiveInfoCleaner.exe", "Error",
+                    MB_OK | MB_ICONERROR);
         return;
     }
 
@@ -132,7 +127,8 @@ void RunSensitiveInfoCleaner()
 
     // Read output.txt as UTF-8 and display content in outputBox
     std::ifstream outputFile("output.txt", std::ios::binary);
-    std::string outputContentUtf8((std::istreambuf_iterator<char>(outputFile)), std::istreambuf_iterator<char>());
+    std::string outputContentUtf8((std::istreambuf_iterator<char>(outputFile)),
+                                  std::istreambuf_iterator<char>());
     outputFile.close();
 
     std::wstring outputContentW = utf8_conv.from_bytes(outputContentUtf8);
